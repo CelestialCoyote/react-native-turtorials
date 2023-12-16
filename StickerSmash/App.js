756cbb80e1e5b652516e41ff247fa51c20a9d09b
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2023-12-16 14:36:39 -0800

    first screen work

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,35 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
 
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
-      </View>
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<ImageViewer placeholderImageSource={PlaceholderImage} />
+			</View>
 
-      <StatusBar style='auto' />
-    </View>
-  );
-}
+			<View style={styles.footerContainer}>
+				<Button theme="primary" label="Choose a photo" />
+				<Button label="Use this photo" />
+			</View>
+
+			<StatusBar style='auto' />
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292E',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-    paddingTop: 58,
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#25292E',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: "center"
+	},
 });
