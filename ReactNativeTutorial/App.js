83f957e12339232e79925a6c13becf361a95efbb
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2023-12-24 15:04:52 -0800

    learning styles

--- ReactNativeTutorial/App.js
+++ ReactNativeTutorial/App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<View>
+				<Text>Open up App.js to start working on your app!</Text>
+			</View>
 
-			<StatusBar style="auto" />
+			<Text style={{margin: 16, borderWidth: 2, borderColor: "red", padding: 16}}>
+				Hello World.
+			</Text>
+
+			<Button title="Click Me!" />
 		</View>
 	);
 };
