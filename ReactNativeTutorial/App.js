4caa4ab572d1d9d94abc3ee5c72e42fae2c0c492
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2023-12-24 15:59:33 -0800

    basic styles for default view

--- ReactNativeTutorial/App.js
+++ ReactNativeTutorial/App.js
@@ -12,7 +12,7 @@ export default function App() {
 				<Button title="Add Goal" />
 			</View>
 
-			<View>
+			<View style={styles.goalsContainer}>
 				<Text>
 					List of goals...
 				</Text>
@@ -24,17 +24,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	appContainer: {
-		padding: 50,
+		flex: 1,
+		paddingHorizontal: 16,
+		paddingTop: 50,
 	},
 	inputContainer: {
+		alignItems: "center",
+		borderBottomColor: "#CCCCCC",
+		borderBottomWidth: 1,
+		flex: 1,
 		flexDirection: "row",
-		justifyContent: "space-between"
+		justifyContent: "space-between",
+		marginBottom: 24
 	},
 	textInput: {
-		borderColor: "#CCC",
+		borderColor: "#CCCCCC",
 		borderWidth: 1,
 		marginRight: 8,
 		padding: 8,
-		width: "80%"
+		width: "70%",
+	},
+	goalsContainer: {
+		flex: 5,
 	}
 });
