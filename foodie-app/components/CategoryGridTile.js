767319c5c71dd5a69ed32e2086f96e738e93cbd3
Paul Stearns <paulstearns@Pauls-MacBook-Pro.local>
2024-01-24 18:39:18 -0800

    added first components

---
+++ foodie-app/components/CategoryGridTile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Pressable, Text, View } from "react-native";
+
+
+export default function CategoryGridTile ({ title, color }) {
+    return (
+        <View>
+            <Pressable>
+                <View>
+                    <Text>{title}</Text>
+                </View>
+            </Pressable>
+        </View>
+    );
+};
---
+++ foodie-app/screens/CategoriesScreen.js
@@ -0,0 +1,36 @@
+import { StyleSheet, FlatList, View } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+
+
+const renderCategoryItem = (itemData) => {
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    );
+};
+
+export default function CategoriesScreen() {
+    return (
+        // <View>
+            <FlatList
+                keyExtractor={(item) => item.id}
+                data={CATEGORIES}
+                renderItem={renderCategoryItem}
+            />
+        // </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
